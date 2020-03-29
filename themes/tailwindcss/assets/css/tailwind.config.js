175ae97ade9cbe58c823b7ec9aff1fcba7cdf8c9
meganii <yuhei24@gmail.com>
2020-03-29 14:10:52 +0900

    Update Design

--- themes/tailwindcss/assets/css/tailwind.config.js
+++ themes/tailwindcss/assets/css/tailwind.config.js
@@ -17,7 +17,27 @@ module.exports = {
         'sans-serif'
       ]
     },
-    extend: {}
+    listStyleType:{
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      circle: 'circle',
+    },
+    extend: {
+      colors: {
+        gray: {
+          '100': '#f8f8f8',
+          '200': '#eeeeee',
+          '300': '#e0e0e0',
+          '400': '#bdbdbd',
+          '500': '#9e9e9e',
+          '600': '#757575',
+          '700': '#616161',
+          '800': '#424242',
+          '900': '#212121',
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
